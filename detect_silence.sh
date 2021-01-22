c9fc126b2761da941f4fed6e723706c1e805bf6e
FranciscoOssian <ossian.neto@gmail.com>
2021-01-22 14:38:13 -0300

    works

--- detect_silence.sh
+++ detect_silence.sh
@@ -4,4 +4,4 @@ IN=$1
 THRESH=$2
 DURATION=$3
 
-ffmpeg -hide_banner -vn -i $IN -af "silencedetect=n=${THRESH}dB:d=${DURATION}" -f null - 2>&1 | grep "silence_end" | awk '{print $5 " " $8}' > silence.txt
+ffmpeg -hide_banner -vn -i $IN -af "silencedetect=n=${THRESH}dB:d=${DURATION}" -f null - 2>&1 | grep 'silence_end\|silence_start' | awk '{if(NR%2!=0)printf $5}{if(NR%2==0)printf " " $5 "\n"}' > silence.txt
