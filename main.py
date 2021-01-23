fbedb2ac144560c694e8b653e982778c99626a8e
FranciscoOssian <ossian.neto@gmail.com>
2021-01-23 14:26:40 -0300

    cli

--- main.py
+++ main.py
@@ -1,17 +1,14 @@
 #!/usr/bin/env python
 
 import sys
+import os
 from moviepy.editor import VideoFileClip, concatenate_videoclips
 
-file_in = sys.argv[1]
-file_out = sys.argv[2]
-silence_file = sys.argv[3]
-
-def main():
+def remove_silence(file_in, file_out):
     print("ddfd")
     clips = []
     clip = VideoFileClip(file_in)
-    times = open(silence_file, "r")
+    times = open("silence.txt", "r")
     last_end = 0
     while 1:
         line = times.readline()
@@ -43,5 +40,23 @@ def main():
     times.close()
     clip.close()
 
+os.system("clear")
+print("----------------------EDIT VIDEO-----------------------------")
+print("1) to donwload youtube video")
+print("2) to to remove video silence")
+print("----------------------EDIT VIDEO-----------------------------")
 
-main()
+opc = int(input(":"))
+
+if opc == 1:
+    print("what's the path of file_in")
+    file_in = input(":")
+    print("what's the path of file_out")
+    file_out = input(":")
+    print("qual o limite em deciécibeis para ser definido? (obs, a parte de vídeos que tiver um som abaixo do definido será cortada)")
+    deb = input("decibéis (apenas o valor):")
+    print("qual o limite em segundos para o corte? (obs, não será cortado faixas de tempo menores que esse valor)")
+    seg = input("segundos (apenas o valor):")
+    command = "sh detect_silence.sh " + file_in + " " + deb + " " + seg
+    os.system(command)
+    remove_silence(file_in, file_out)
